0657244f00a9a687ac542c2f46e57bbde846e571
Joan Montoya <joan15montoya@gmail.com>
2023-03-14 18:09:14 -0600

    sec

--- src/app/Services/usuarios.service.ts
+++ src/app/Services/usuarios.service.ts
@@ -6,8 +6,8 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 })
 export class UsuariosService {
 
-  //url = 'https://back-login12.herokuapp.com';
-  url = 'http://localhost:3000';
+  url = 'https://back-ventas12.herokuapp.com';
+  //url = 'http://localhost:3000';
 
   constructor(private http: HttpClient) {}
 
